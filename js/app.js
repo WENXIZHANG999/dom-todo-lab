8ab71ebeab74b00170be7f5fc5be785a8c92faf0
wenxizhang <wenxizhang999@gmail.com>
2022-02-03 00:10:32 -0500

    solve

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('sanity check')
+const btn = document.querySelector('button')
+const inp = document.querySelector('input')
+const newUl = document.querySelector('ul')
+const resetBtn = document.querySelector('#reset-button')
+
+
+btn.addEventListener('click',function(evt){
+  const newLi = document.createElement('li')
+  newLi.textContent = inp.value
+  if(inp.value !==' '){
+    document.querySelector('ul').appendChild(newLi)
+    inp.value=''
+  }
+})
+resetBtn.addEventListener('click',function(evt){
+  newUl.innerHTML = ''
+  inp.value=''
+})
